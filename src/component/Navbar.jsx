89eef09ec3d9a214307be3c203a9923b41cb1c71
damola ifatifo <ifatifod4@gmail.com>
2024-02-07 12:18:54 +0100

    login-update

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,24 +1,36 @@
 import React from 'react'
 import { useFormik } from 'formik'
+import * as yup from "yup"
 const Navbar = () => {
-  const formik = useFormik({
-       initialValues:{
-        firstname:"",
-        lastname:"",
-        email:"",
-        password:"",
-       },
-       onSubmit : (values)=>{
-         console.log(values);
-       }
-    })
+  const validationSchema = yup.object({
+    firstname: yup.string().required("Required").max(10).min(3),
+    lastname: yup.string().required("Required").max(10).min(3),
+    email: yup.string().email("valid email address"),
+    password: yup.string().matches(/^[a-zA-z0-9]{8}$/, "password must be atleast 8 characters, uppercase,lowercase and a special character "),
+  })
+  const { handleChange, handleSubmit, values, errors } = useFormik({
+    initialValues: {
+      firstname: "",
+      lastname: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    }
+  })
   return (
     <div>
-      <form action="" onSubmit={formik.handleSubmit}>
-        <input type="text" onChange={formik.handleChange } placeholder='firstname' value={formik.values.firstname} name='firstname'  />
-        <input type="text" onChange={formik.handleChange } placeholder='lastname' value={formik.values.lastname} name='lastname' />
-        <input type="email" onChange={formik.handleChange } placeholder='email'value={formik.values.email} name='email' />
-        <input type="password" onChange={formik.handleChange }  placeholder='password' value={formik.values.password} name='password' />
+      <form action="" onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange} placeholder='firstname' value={values.firstname} name='firstname' />
+        <span>{errors.firstname}</span>
+        <input type="text" onChange={handleChange} placeholder='lastname' value={values.lastname} name='lastname' />
+        <span>{errors.lastname}</span>
+        <input type="email" onChange={handleChange} placeholder='email' value={values.email} name='email' />
+        <span>{errors.email}</span>
+        <input type="password" onChange={handleChange} placeholder='password' value={values.password} name='password' />
+        <span>{errors.password}</span>
         <button type='submit'>submit</button>
       </form>
     </div>
