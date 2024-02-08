8e87c89cce44666a170b9260c84f237dcefbe479
damola ifatifo <ifatifod4@gmail.com>
2024-02-08 09:57:41 +0100

    update

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -22,16 +22,38 @@ const Navbar = () => {
   })
   return (
     <div>
-      <form action="" onSubmit={handleSubmit}>
-        <input type="text" onChange={handleChange} placeholder='firstname' value={values.firstname} name='firstname' />
-        <span>{errors.firstname}</span>
-        <input type="text" onChange={handleChange} placeholder='lastname' value={values.lastname} name='lastname' />
-        <span>{errors.lastname}</span>
-        <input type="email" onChange={handleChange} placeholder='email' value={values.email} name='email' />
-        <span>{errors.email}</span>
-        <input type="password" onChange={handleChange} placeholder='password' value={values.password} name='password' />
-        <span>{errors.password}</span>
-        <button type='submit'>submit</button>
+
+      <form class="form card" onSubmit={handleSubmit}>
+        <div class="card_header">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+            <path fill="none" d="M0 0h24v24H0z"></path>
+            <path fill="currentColor" d="M4 15h2v5h12V4H6v5H4V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6zm6-4V8l5 4-5 4v-3H2v-2h8z"></path>
+          </svg>
+          <h1 class="form_heading">Sign in</h1>
+        </div>
+        <div class="field">
+          <label for="firstname">firstname</label>
+          <input class="input" onChange={handleChange} name="firstname" type="text" value={values.firstname} placeholder="firstname" id="username" />
+          <span>{errors.firstname}</span>
+        </div>
+        <div class="field">
+          <label for="lastname">lastname</label>
+          <input class="input" onChange={handleChange} name="lastname" type="text" value={values.lastname} placeholder="lastname" id="username" />
+          <span>{errors.lastname}</span>
+        </div>
+        <div class="field">
+          <label for="email">email</label>
+          <input class="input" onChange={handleChange} name="email" type="text" value={values.email} placeholder="email" id="username" />
+          <span>{errors.email}</span>
+        </div>
+        <div class="field">
+          <label for="password">Password</label>
+          <input class="input" onChange={handleChange} name="password" value={values.password} type="password" placeholder="Password" id="password" />
+          <span>{errors.password}</span>
+        </div>
+        <div class="field">
+          <button type='submit' class="button">Login</button>
+        </div>
       </form>
     </div>
   )
